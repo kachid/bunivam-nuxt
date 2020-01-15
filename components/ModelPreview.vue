1f7de5685ed82e9ac0e767a00636999ead3e7e40
Mikhail Kakhidze <kachid76@gmail.com>
2020-01-15 17:00:30 +0500

    fix style ModelPreview page

--- components/ModelPreview.vue
+++ components/ModelPreview.vue
@@ -1,26 +1,29 @@
 <template>
   <v-card :to="`/models/${model.id}`" max-width="350" height="420" nuxt hover>
     <v-card-title class="justify-center">{{ model.name }}</v-card-title>
+    <v-card-subtitle class="text-center"
+      >от {{ model.price }} руб.</v-card-subtitle
+    >
     <v-img
       :src="model.src2"
       :srcset="model.src1"
       :alt="model.description"
-      max-width="300"
+      max-width="350"
       max-height="300"
     />
-    <v-card-text class="text-center">от {{ model.price }} руб.</v-card-text>
   </v-card>
 </template>
 
 <script>
 export default {
+  name: 'ModelPreview',
   props: {
-    // eslint-disable-next-line vue/require-default-prop
     model: {
+      // eslint-disable-next-line vue/require-default-prop
       type: Object
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
--- pages/models/index.vue
+++ pages/models/index.vue
@@ -1,18 +1,17 @@
 <template>
   <v-row>
     <v-col v-for="model in modelList" :key="model.id">
-      <AppBriefModel :model="model" />
+      <app-model-preview :model="model" />
     </v-col>
   </v-row>
 </template>
 
 <script>
-import AppBriefModel from '@/components/BriefModel'
+import AppModelPreview from '@/components/ModelPreview'
 
 export default {
-  name: 'Models',
   components: {
-    AppBriefModel
+    AppModelPreview
   },
   data: () => ({
     modelList: [
