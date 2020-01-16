8dba8f8ba77b91ea054e98eb47c463d1a19d307d
Mikhail Kakhidze <kachid76@gmail.com>
2020-01-16 14:56:06 +0500

    put all data in a separate file

--- pages/models/index.vue
+++ pages/models/index.vue
@@ -9,45 +9,14 @@
 <script>
 import AppModelPreview from '@/components/ModelPreview'
 
+import { modelList } from '@/data'
+
 export default {
   components: {
     AppModelPreview
   },
   data: () => ({
-    modelList: [
-      {
-        id: 1,
-        name: 'Ракушка',
-        description: 'Ракушка',
-        price: 139000,
-        src1: '/images/rakush_card_1_small.png',
-        src2: '/images/rakush_card_1_small.webp'
-      },
-      {
-        id: 2,
-        name: 'Скандинавия',
-        description: 'Скандинавия',
-        price: 195000,
-        src1: '/images/scand_card_small.png',
-        src2: '/images/scand_card_small.webp'
-      },
-      {
-        id: 3,
-        name: 'Каравелла',
-        description: 'Каравелла',
-        price: 129000,
-        src1: '/images/Каравелла_card_small.png',
-        src2: '/images/Каравелла_card_small.webp'
-      },
-      {
-        id: 4,
-        name: 'Радуга',
-        description: 'Радуга',
-        price: 74000,
-        src1: './images/1%20Радуга.jpg',
-        src2: './images/1%20Радуга.jpg'
-      }
-    ]
+    modelList
   })
 }
 </script>
